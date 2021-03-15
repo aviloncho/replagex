f0d980d84efab1cc35ddba7cdf07a057c0961e89
Juan Sebastian Avila <sebas.aviloncho@gmail.com>
2021-03-15 16:37:30 -0500

    Pattern: Include ignore_case & multiline properties. Include it into __init__ with default False value. Create flags method for Regex flags according to self properties.
    Replagex: load_from_json_file read ignore_case & multiline values.
    apply_regex, uses Pattern.flags method for re.compile.

--- src/replagex/pattern/pattern.py
+++ src/replagex/pattern/pattern.py
@@ -1,10 +1,29 @@
+import re
+
 
 class Pattern:
     regex: str = ''
     replace: str = ''
     dotall: bool = False
+    ignore_case: bool = False
+    multiline: bool = False
 
-    def __init__(self, regex: str, replace: str, dotall: bool = False):
+    def __init__(self, regex: str, replace: str, dotall: bool = False,
+                 ignore_case: bool = False, multiline: bool = False):
         self.regex = str(regex)
         self.replace = str(replace).replace("$", "\\")
         self.dotall = dotall
+        self.ignore_case = ignore_case
+        self.multiline = multiline
+
+    def flags(self):
+        flags = 0
+
+        if self.dotall:
+            flags += re.DOTALL
+        if self.ignore_case:
+            flags += re.IGNORECASE
+        if self.multiline:
+            flags += re.MULTILINE
+
+        return flags
